cd5c09b6eb52cce530fedee1e1a93ddbd51bdb83
UweHennig <uh@uwe-hennig.com>
2026-06-26 09:58:56 +0200

    adding AgentMediator stub

---
+++ snn-afferent/src/main/java/com/uwe_hennig/snn/afferent/agent/AgentMediator.java
@@ -0,0 +1,15 @@
+/**
+ * @(#)AgentMediator.java
+ * Copyright (c) 2026 Uwe Hennig
+ * All rights reserved.
+ */
+package com.uwe_hennig.snn.afferent.agent;
+
+/**
+ * AgentMediator
+ *
+ * @author Uwe Hennig
+ */
+public abstract class AgentMediator {
+
+}
