fba4b48331d3de01c8720cb42e08766cdce5d381
UweHennig <uh@uwe-hennig.com>
2026-06-28 11:52:41 +0200

    adding SnnEffector port

---
+++ snn-afferent/src/main/java/com/uwe_hennig/snn/afferent/agent/SnnEffectorImpl.java
@@ -0,0 +1,46 @@
+/**
+ * @(#)SnnEffectorImpl.java
+ * Copyright (c) 2026 Uwe Hennig
+ * All rights reserved.
+ */
+package com.uwe_hennig.snn.afferent.agent;
+
+import com.uwe_hennig.snn.contracts.afferent.EffectorConsumer;
+import com.uwe_hennig.snn.contracts.afferent.SnnEffector;
+import com.uwe_hennig.snn.services.StimulusService;
+
+/**
+ * SnnEffectorImpl
+ *
+ * @author Uwe Hennig
+ */
+public class SnnEffectorImpl implements SnnEffector {
+    private final int identifier;
+
+    private EffectorConsumer consumer;
+
+    private SnnEffectorImpl(int identifier) {
+        this.identifier = identifier;
+    }
+
+    public static SnnEffector of(int identifier) {
+        return new SnnEffectorImpl(identifier);
+    }
+
+    public void stimulate(int stimulusIdentifier) {
+        if (consumer != null) {
+            float value = StimulusService.getValue(stimulusIdentifier);
+            consumer.accept(this, value);
+        }
+    }
+
+    @Override
+    public long getIdentifier() {
+        return identifier;
+    }
+
+    @Override
+    public void withConsumer(EffectorConsumer consumer) {
+        this.consumer = consumer;
+    }
+}
