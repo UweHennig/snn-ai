e8cdf1511c7ebac8df3a41a6f91bd93ec663e3d1
UweHennig <uh@uwe-hennig.com>
2026-06-28 13:24:32 +0200

    adding peripheral interfaces

---
+++ snn-contracts/src/main/java/com/uwe_hennig/snn/contracts/afferent/EnvironmentPeripheral.java
@@ -0,0 +1,19 @@
+/**
+ * @(#)EnvironmentPeripheral.java
+ * Copyright (c) 2026 Uwe Hennig
+ * All rights reserved.
+ */
+package com.uwe_hennig.snn.contracts.afferent;
+
+import java.util.List;
+
+/**
+ * EnvironmentPeripheral
+ *
+ * @author Uwe Hennig
+ */
+public interface EnvironmentPeripheral {
+    List<EnvAction<?>> getEnvActions();
+    List<EnvState<?>> getEnvState();
+    List<EnvFeedback<?>> getEnvFeedback();
+}
---
+++ snn-contracts/src/main/java/com/uwe_hennig/snn/contracts/afferent/NeuroPeripheral.java
@@ -0,0 +1,19 @@
+/**
+ * @(#)Peripheral.java
+ * Copyright (c) 2026 Uwe Hennig
+ * All rights reserved.
+ */
+package com.uwe_hennig.snn.contracts.afferent;
+
+import java.util.List;
+
+/**
+ * Peripheral
+ *
+ * @author Uwe Hennig
+ */
+public interface NeuroPeripheral {
+    public List<SnnReceptor> getReceptors();
+    public List<SnnEffector> getEffectors();
+    public List<SnnFeedback> getFeedbacks();
+}
