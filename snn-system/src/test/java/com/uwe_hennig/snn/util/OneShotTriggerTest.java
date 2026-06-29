0f9a5ce379492227ac56b02c0c4801a05958b11f
UweHennig <uh@uwe-hennig.com>
2026-06-30 00:30:19 +0200

    adding OneShotTriggerTest

---
+++ snn-system/src/test/java/com/uwe_hennig/snn/util/OneShotTriggerTest.java
@@ -0,0 +1,53 @@
+/**
+ * @(#)OneShotTriggerTest.java
+ * Copyright (c) 2026 Uwe Hennig
+ * All rights reserved.
+ */
+package com.uwe_hennig.snn.util;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+/**
+ * OneShotTriggerTest
+ * @formatter:off
+ * @formatter:on
+ * @author Uwe Hennig
+ */
+public class OneShotTriggerTest {
+
+    @Test
+    @DisplayName("Simple OneShotTrigger wait test")
+    void testSimpleOneShotWait() {
+        long start = System.currentTimeMillis();
+        OneShotTrigger ost = OneShotTrigger.of(Duration.ofSeconds(1));
+        ost.waitOnSignal();
+        long end = System.currentTimeMillis();
+        System.out.println("" + (end - start) + " msec");
+        assertTrue(end - start >= 999, "Invalid wait time!");
+    }
+    @Test
+
+    @DisplayName("Simple OneShotTrigger run test")
+    void testSimpleOneShotExecute() {
+        long start = System.currentTimeMillis();
+        OneShotTrigger ost = OneShotTrigger.of(Duration.ofSeconds(1));
+        long [] end = {0};
+        ost.executeOnSignal(() -> end[0]=System.currentTimeMillis());
+        System.out.println((end[0] - start)  + " msec");
+        assertTrue(end[0] - start >= 999, "Invalid execution time!");
+    }
+
+    @BeforeEach
+    void setUp(TestInfo info) {
+        String title = "### " + info.getDisplayName() + " ###";
+        System.out.println("\n" + title);
+        System.out.println("-".repeat(title.length()));
+    }
+}
