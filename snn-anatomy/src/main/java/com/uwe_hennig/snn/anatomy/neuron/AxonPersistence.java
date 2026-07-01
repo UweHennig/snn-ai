facce4aa2edc7f164ad991db58e1fbbb59185f2e
UweHennig <uh@uwe-hennig.com>
2026-07-01 19:24:32 +0200

    adding persistence to all models

---
+++ snn-anatomy/src/main/java/com/uwe_hennig/snn/anatomy/neuron/AxonPersistence.java
@@ -0,0 +1,53 @@
+/**
+ * @(#)AxonPersistence.java
+ * Copyright (c) 2026 Uwe Hennig
+ * All rights reserved.
+ */
+package com.uwe_hennig.snn.anatomy.neuron;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.READ;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
+import static java.nio.file.StandardOpenOption.WRITE;
+
+import java.io.IOException;
+import java.lang.foreign.Arena;
+import java.lang.foreign.MemorySegment;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+
+/**
+ * AxonPersistence
+ *
+ * @author Uwe Hennig
+ */
+public class AxonPersistence {
+    public static void save(AxonModel model, Path file) throws IOException {
+        try (FileChannel channel = FileChannel.open(file, CREATE, READ, WRITE, TRUNCATE_EXISTING)) {
+            int size = (int) model.segment.byteSize();
+
+            try (Arena tempArena = Arena.ofConfined()) {
+                MemorySegment mappedFile = channel.map(FileChannel.MapMode.READ_WRITE, 0, size, tempArena);
+                mappedFile.copyFrom(model.segment);
+                mappedFile.force();
+            }
+        }
+
+    }
+
+    public static AxonModel laod(Path file) throws IOException {
+        try (FileChannel channel = FileChannel.open(file, READ)) {
+            int size = (int) channel.size();
+            int capacity = size / (int) AxonModel.LAYOUT.byteSize();
+            AxonModel model = new AxonModel(capacity);
+
+            try (Arena tempArena = Arena.ofConfined()) {
+                MemorySegment mappedFile = channel.map(FileChannel.MapMode.READ_ONLY, 0, size, tempArena);
+                model.segment.copyFrom(mappedFile);
+            }
+
+            return model;
+        }
+
+    }
+}
---
+++ snn-anatomy/src/main/java/com/uwe_hennig/snn/anatomy/neuron/NeuronPersistence.java
@@ -0,0 +1,51 @@
+/**
+ * @(#)NeuronPersistence.java
+ * Copyright (c) 2026 Uwe Hennig
+ * All rights reserved.
+ */
+package com.uwe_hennig.snn.anatomy.neuron;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.READ;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
+import static java.nio.file.StandardOpenOption.WRITE;
+
+import java.io.IOException;
+import java.lang.foreign.Arena;
+import java.lang.foreign.MemorySegment;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+
+/**
+ * NeuronPersistence
+ *
+ * @author Uwe Hennig
+ */
+public class NeuronPersistence {
+    public static void save(NeuronModel model, Path file) throws IOException {
+        try (FileChannel channel = FileChannel.open(file, CREATE, READ, WRITE, TRUNCATE_EXISTING)) {
+            int size = (int) model.segment.byteSize();
+
+            try (Arena tempArena = Arena.ofConfined()) {
+                MemorySegment mappedFile = channel.map(FileChannel.MapMode.READ_WRITE, 0, size, tempArena);
+                mappedFile.copyFrom(model.segment);
+                mappedFile.force();
+            }
+        }
+    }
+
+    public static NeuronModel laod(Path file) throws IOException {
+        try (FileChannel channel = FileChannel.open(file, READ)) {
+            int size = (int) channel.size();
+            int capacity = size / (int) NeuronModel.LAYOUT.byteSize();
+            NeuronModel model = new NeuronModel(capacity);
+
+            try (Arena tempArena = Arena.ofConfined()) {
+                MemorySegment mappedFile = channel.map(FileChannel.MapMode.READ_ONLY, 0, size, tempArena);
+                model.segment.copyFrom(mappedFile);
+            }
+
+            return model;
+        }
+    }
+}
