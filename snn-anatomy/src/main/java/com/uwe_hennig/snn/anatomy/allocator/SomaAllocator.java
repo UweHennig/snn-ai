4b386aa06defe2828cff2abc7aec42e8c9140734
UweHennig <uh@uwe-hennig.com>
2026-07-01 16:38:48 +0200

    adding SomaAllocator

---
+++ snn-anatomy/src/main/java/com/uwe_hennig/snn/anatomy/allocator/SomaAllocator.java
@@ -0,0 +1,83 @@
+/**
+ * @(#)SomaAllocator.java
+ * Copyright (c) 2026 Uwe Hennig
+ * All rights reserved.
+ */
+package com.uwe_hennig.snn.anatomy.allocator;
+
+import com.uwe_hennig.snn.anatomy.neuron.PlasticityModel;
+import com.uwe_hennig.snn.anatomy.neuron.PlasticityView;
+import com.uwe_hennig.snn.anatomy.neuron.PotentialModel;
+import com.uwe_hennig.snn.anatomy.neuron.PotentialView;
+import com.uwe_hennig.snn.anatomy.neuron.SomaModel;
+import com.uwe_hennig.snn.anatomy.neuron.SomaView;
+import com.uwe_hennig.snn.anatomy.neuron.ThresholdModel;
+import com.uwe_hennig.snn.anatomy.neuron.ThresholdView;
+import com.uwe_hennig.snn.anatomy.neuron.WeightModel;
+
+/**
+ * SomaAllocator
+ *
+ * @author Uwe Hennig
+ */
+public class SomaAllocator {
+    private static SomaAllocator INSTANCE;
+
+    private final SomaModel model;
+    private final PotentialModel potentialModel;
+    private final ThresholdModel thresholdModel;
+    private final PlasticityModel ltpModel;
+    private final PlasticityModel stpModel;
+
+    private int nextOffset = 0;
+
+    private SomaAllocator(int capacity) {
+        this.model = new SomaModel(capacity);
+        this.potentialModel = new PotentialModel(capacity);
+        this.thresholdModel = new ThresholdModel(capacity);
+        this.ltpModel = new PlasticityModel(capacity);
+        this.stpModel = new PlasticityModel(capacity);
+    }
+
+    public static SomaAllocator initInstance(int capacity) {
+        INSTANCE = new SomaAllocator(capacity);
+        return INSTANCE;
+    }
+
+    public static SomaAllocator instance() {
+        return INSTANCE;
+    }
+
+    public SomaView newSomaView(int fieldId, int neuronId, int axonId) {
+        if (nextOffset >= model.getCapacity()) {
+            throw new IllegalStateException("Out of Offheap memory");
+        }
+
+        int offset = nextOffset++;
+
+        PotentialView potentialView = new PotentialView(offset, potentialModel);
+        ThresholdView thresholdView = new ThresholdView(offset, thresholdModel);
+        PlasticityView stpView = new PlasticityView(offset, stpModel);
+        PlasticityView ltpView = new PlasticityView(offset, ltpModel);
+
+        SomaView somaView = new SomaView(offset, model, potentialView, thresholdView, stpView, ltpView);
+        somaView.setStructure(fieldId, neuronId, axonId);
+
+        return somaView;
+    }
+
+    public SomaView viewAt(int viewId) {
+        // TODO capacity check and resuse in newSomaView
+        if (viewId >= nextOffset) {
+            return null;
+        }
+
+        PotentialView potentialView = new PotentialView(viewId, potentialModel);
+        ThresholdView thresholdView = new ThresholdView(viewId, thresholdModel);
+        PlasticityView stpView = new PlasticityView(viewId, stpModel);
+        PlasticityView ltpView = new PlasticityView(viewId, ltpModel);
+
+        SomaView somaView = new SomaView(viewId, model, potentialView, thresholdView, stpView, ltpView);
+        return somaView;
+    }
+}
--- snn-anatomy/src/main/java/com/uwe_hennig/snn/anatomy/neuron/SomaView.java
+++ snn-anatomy/src/main/java/com/uwe_hennig/snn/anatomy/neuron/SomaView.java
@@ -12,44 +12,21 @@ package com.uwe_hennig.snn.anatomy.neuron;
 public final class SomaView {
     private final int       index;
     private final SomaModel model;
+    private final PotentialView potentialView;
+    private final ThresholdView thresholdView;
+    private final PlasticityView stpView;
+    private final PlasticityView ltpView;
 
-    private final int fieldId;
-    private final int neuronId;
-    private final int potentialId;
-    private final int thresholdId;
-    private final int stpId;
-    private final int ltpId;
-
-    public SomaView(int index, SomaModel model, int fieldId, int neuronId, int potentialId, int thresholdId, int stpId, int ltpId) {
+    public SomaView(int index, SomaModel model, PotentialView potentialView, ThresholdView thresholdView, PlasticityView stpView, PlasticityView ltpView) {
         assert model != null : "Model must not be null!";
         assert index < model.capacity && index >= 0 : " " + index + " >= " + model.capacity;
 
         this.index = index;
         this.model = model;
-        this.fieldId = fieldId;
-        this.neuronId = neuronId;
-
-        this.potentialId = potentialId;
-        this.thresholdId = thresholdId;
-        this.stpId = stpId;
-        this.ltpId = ltpId;
-
-        initData();
-    }
-
-    // TODO remove
-    private void initData() {
-        try {
-            model.writeLock(index);
-            model.setFieldId(index, fieldId);
-            model.setNeuronId(index, neuronId);
-            model.setPotentialId(index, potentialId);
-            model.setThresholdId(index, thresholdId);
-            model.setStpId(index, stpId);
-            model.setLtpId(index, ltpId);
-        } finally {
-            model.writeUnlock(index);
-        }
+        this.potentialView = potentialView;
+        this.thresholdView = thresholdView;
+        this.stpView = stpView;
+        this.ltpView = ltpView;
     }
 
     public SomaModel getModel() {
@@ -60,31 +37,27 @@ public final class SomaView {
         return index;
     }
 
-    public int getFieldId() {
-        return model.getFieldId(index);
-    }
-
     public int getNeuronId() {
         return model.getNeuronId(index);
     }
 
-    public int getPotentialId() {
-        return model.getPotentialId(index);
-    }
-
-    public int getThresholdId() {
-        return model.getThresholdId(index);
-    }
-
-    public int getStp() {
-        return model.getStpId(index);
-    }
-
-    public int getLtpId() {
-        return model.getLtpId(index);
-    }
-
     public int getAxonId() {
         return model.getAxonId(index);
+    }
+
+    public void setStructure(int fieldId, int neuronId, int axonId) {
+        model.writeLock(index);
+        try {
+            model.setFieldId(index, fieldId);
+            model.setNeuronId(index, neuronId);
+            model.setAxonId(index, axonId);
+
+            model.setPotentialId(index, potentialView.getViewId());
+            model.setThresholdId(index, thresholdView.getViewId());
+            model.setStpId(index, stpView.getViewId());
+            model.setLtpId(index, ltpView.getViewId());
+        } finally {
+            model.writeUnlock(index);
+        }
     }
 }
