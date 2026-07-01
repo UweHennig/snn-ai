4555ad47e7fdb4300d8cfa15466c6e576e40d5fa
UweHennig <uh@uwe-hennig.com>
2026-07-01 15:51:44 +0200

    adding DendritAllocator

---
+++ snn-anatomy/src/main/java/com/uwe_hennig/snn/anatomy/allocator/DendritAllocator.java
@@ -0,0 +1,54 @@
+/**
+ * @(#)DendritAllocator.java
+ * Copyright (c) 2026 Uwe Hennig
+ * All rights reserved.
+ */
+package com.uwe_hennig.snn.anatomy.allocator;
+
+import com.uwe_hennig.snn.anatomy.neuron.DendritModel;
+import com.uwe_hennig.snn.anatomy.neuron.DendritView;
+import com.uwe_hennig.snn.anatomy.neuron.WeightModel;
+import com.uwe_hennig.snn.anatomy.neuron.WeightView;
+
+/**
+ * DendritAllocator
+ *
+ * @author Uwe Hennig
+ */
+public class DendritAllocator {
+    private static DendritAllocator INSTANCE;
+
+    private final DendritModel model;
+    private final WeightModel  weightModel;
+
+    private int nextOffset = 0;
+
+    private DendritAllocator(int capacity) {
+        model = new DendritModel(capacity);
+        weightModel = new WeightModel(capacity);
+    }
+
+    public static DendritAllocator initInstance(int capacity) {
+        INSTANCE = new DendritAllocator(capacity);
+        return INSTANCE;
+    }
+
+    public static DendritAllocator instance() {
+        return INSTANCE;
+    }
+
+    public DendritView newDendritView(int fieldId, int neuronId, int somaId) {
+        if (nextOffset >= model.getCapacity()) {
+            throw new IllegalStateException("Out of Offheap memory");
+        }
+
+        int offset = nextOffset++;
+
+        WeightView weightView = new WeightView(offset, weightModel);
+        DendritView view = new DendritView(offset, model, weightView);
+        view.setStructure(fieldId, neuronId, somaId);
+
+        return view;
+    }
+
+}
--- snn-anatomy/src/main/java/com/uwe_hennig/snn/anatomy/neuron/DendritView.java
+++ snn-anatomy/src/main/java/com/uwe_hennig/snn/anatomy/neuron/DendritView.java
@@ -15,35 +15,14 @@ public final class DendritView {
     private final DendritModel model;
 
     private WeightView weightView;
-    private int        fieldId;
-    private int        neuronId;
-    private int        somaId;
 
-    public DendritView(int index, DendritModel model, WeightView weightView, int fieldId, int neuronId, int somaId) {
+    public DendritView(int index, DendritModel model, WeightView weightView) {
         assert model != null : "Model must not be null!";
         assert index < model.capacity && index >= 0 : " " + index + " >= " + model.capacity;
 
         this.index = index;
         this.model = model;
-        this.fieldId = fieldId;
-        this.neuronId = neuronId;
-        this.somaId = somaId;
         this.weightView = weightView;
-
-        initData();
-    }
-
-    // TODO remove
-    private void initData() {
-        try {
-            model.writeLock(index);
-            model.setFieldId(index, fieldId);
-            model.setNeuronId(index, neuronId);
-            model.setSomaId(index, somaId);
-            model.setWeightId(index, weightView.getViewId());
-        } finally {
-            model.writeUnlock(index);
-        }
     }
 
     // ----- getter/setter -----
@@ -61,15 +40,25 @@ public final class DendritView {
     }
 
     public int getFieldId() {
-        return fieldId;
+        return model.getFiedlId(index);
     }
 
     public int getNeuronId() {
-        return neuronId;
+        return model.getNeuronId(index);
     }
 
     public int getSomaId() {
-        return somaId;
+        return model.getSomaId(index);
     }
 
+    public void setStructure(int fieldId, int neuronId, int saomaId) {
+        model.writeLock(index);
+        try {
+            model.setFieldId(index, fieldId);
+            model.setNeuronId(index, neuronId);
+            model.setSomaId(index, saomaId);
+        } finally {
+            model.writeUnlock(index);
+        }
+    }
 }
