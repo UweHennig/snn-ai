4fb9510cb7cf220c1069a4917c635efd100310a3
UweHennig <uh@uwe-hennig.com>
2026-07-01 17:32:12 +0200

    adding SynapseAllocator

---
+++ snn-anatomy/src/main/java/com/uwe_hennig/snn/anatomy/allocator/SynapseAllocator.java
@@ -0,0 +1,64 @@
+/**
+ * @(#)SynapseAllocator.java
+ * Copyright (c) 2026 Uwe Hennig
+ * All rights reserved.
+ */
+package com.uwe_hennig.snn.anatomy.allocator;
+
+import com.uwe_hennig.snn.anatomy.neuron.ModulatorModel;
+import com.uwe_hennig.snn.anatomy.neuron.ModulatorView;
+import com.uwe_hennig.snn.anatomy.neuron.SynapseModel;
+import com.uwe_hennig.snn.anatomy.neuron.SynapseView;
+
+/**
+ * SynapseAllocator
+ *
+ * @author Uwe Hennig
+ */
+public class SynapseAllocator {
+    private static SynapseAllocator INSTANCE;
+
+    private final SynapseModel model;
+    private final ModulatorModel modulatorModel;
+
+    private int nextOffset = 0;
+
+    private SynapseAllocator(int capacity) {
+        model = new SynapseModel(capacity);
+        modulatorModel = new ModulatorModel(capacity);
+    }
+
+    public static SynapseAllocator initInstance(int capacity) {
+        INSTANCE = new SynapseAllocator(capacity);
+        return INSTANCE;
+    }
+
+    public static SynapseAllocator instance() {
+        return INSTANCE;
+    }
+
+    public SynapseView newSynapseView(int fieldId, int neuronId) {
+        if (nextOffset >= model.getCapacity()) {
+            throw new IllegalStateException("Out of Offheap memory");
+        }
+
+        int offset = nextOffset++;
+
+        ModulatorView modulatorView = new ModulatorView(offset, modulatorModel);
+        SynapseView view = new SynapseView(offset, model, modulatorView);
+
+        view.setStructure(fieldId, neuronId, -1, fieldId);
+        return view;
+    }
+
+    public SynapseView viewAt(int viewId) {
+        if (viewId >= nextOffset) {
+            return null;
+        }
+
+        ModulatorView modulatorView = new ModulatorView(viewId, modulatorModel);
+        SynapseView view = new SynapseView(viewId, model, modulatorView);
+
+        return view;
+    }
+}
--- snn-anatomy/src/main/java/com/uwe_hennig/snn/anatomy/neuron/SynapseView.java
+++ snn-anatomy/src/main/java/com/uwe_hennig/snn/anatomy/neuron/SynapseView.java
@@ -11,39 +11,17 @@ package com.uwe_hennig.snn.anatomy.neuron;
  * @author Uwe Hennig
  */
 public class SynapseView {
-    private final int          index;
-    private final SynapseModel model;
+    private final int           index;
+    private final SynapseModel  model;
+    private final ModulatorView modulatorView;
 
-    private final int fieldId;
-    private final int neuronId;
-    private final int targetId;
-    private final int modulatorId;
-
-    public SynapseView(int index, SynapseModel model, int fieldId, int neuronId, int targetId, int modulatorId) {
+    public SynapseView(int index, SynapseModel model, ModulatorView modulatorView) {
         assert model != null : "Model must not be null!";
         assert index < model.capacity && index >= 0 : " " + index + " >= " + model.capacity;
 
         this.index = index;
         this.model = model;
-        this.fieldId = fieldId;
-        this.neuronId = neuronId;
-        this.targetId = targetId;
-        this.modulatorId = modulatorId;
-
-        initData();
-    }
-
-    // TODO remove
-    private void initData() {
-        try {
-            model.writeLock(index);
-            model.setFieldId(index, fieldId);
-            model.setNeuronId(index, neuronId);
-            model.setTargetId(index, targetId);
-            model.setModulatorId(index, modulatorId);
-        } finally {
-            model.writeUnlock(index);
-        }
+        this.modulatorView = modulatorView;
     }
 
     public SynapseModel getModel() {
@@ -74,4 +52,15 @@ public class SynapseView {
         return model.getModulatorId(index);
     }
 
+    public void setStructure(int fieldId, int neuronIde, int targetId, int modulatorId) {
+        model.writeLock(index);
+        try {
+            model.setFieldId(index, fieldId);
+            model.setNeuronId(index, neuronIde);
+            model.setTargetId(index, targetId);
+            model.setModulatorId(index, modulatorId);
+        } finally {
+            model.writeUnlock(index);
+        }
+    }
 }
