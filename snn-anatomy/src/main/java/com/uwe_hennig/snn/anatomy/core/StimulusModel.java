90e2b98e05b04fbf5b30d70d565219780bc1c913
UweHennig <uh@uwe-hennig.com>
2026-07-08 19:41:47 +0200

    adding better lock to StimulusModel

--- snn-anatomy/src/main/java/com/uwe_hennig/snn/anatomy/core/StimulusModel.java
+++ snn-anatomy/src/main/java/com/uwe_hennig/snn/anatomy/core/StimulusModel.java
@@ -63,15 +63,37 @@ public class StimulusModel {
 
     // ----- lock/unlock -----
 
+    private static final int WRITER_WAITING = 0x40000000; // Bit 30
+    private static final int WRITER_ACTIVE  = 0xFFFFFFFF; // -1
+
     void writeLock(int index) {
         int spins = 0;
-        while (!VH_LOCK.compareAndSet(segment, 0L, index, -1)) {
-            if (spins < 64) {
-                Thread.onSpinWait();
-                spins++;
+        // set the WRITER_WAITING flag to indicate a write request
+        while (true) {
+            int current = (int) VH_LOCK.getVolatile(segment, 0L, index);
+            if ((current & WRITER_WAITING) == 0) {
+                if (VH_LOCK.compareAndSet(segment, 0L, index, current, current | WRITER_WAITING)) {
+                    break;
+                }
             } else {
-                LockSupport.parkNanos(1);
+                break;
             }
+            backoff(spins++);
+        }
+
+        // set the lock
+        spins = 0;
+        while (true) {
+            int current = (int) VH_LOCK.getVolatile(segment, 0L, index);
+
+            // no reader aktive and write flag is set or initial state.
+            if (current == WRITER_WAITING || current == 0) {
+                if (VH_LOCK.compareAndSet(segment, 0L, index, current, WRITER_ACTIVE)) {
+                    return;
+                }
+            }
+
+            backoff(spins++);
         }
     }
 
@@ -79,13 +101,43 @@ public class StimulusModel {
         VH_LOCK.setRelease(segment, 0L, index, 0);
     }
 
+    boolean readLock(int index) {
+        int spins = 0;
+        while (true) {
+            int current = (int) VH_LOCK.getVolatile(segment, 0L, index);
 
-    public boolean tryWriteLock(long index) {
-        return (int) VH_LOCK.compareAndExchange(segment, 0L, index, 0, 1) == 0;
+            // give priority to the writers
+            if (current < 0 || (current & WRITER_WAITING) != 0) {
+                backoff(spins++);
+                continue;
+            }
+
+            // increment reader counter
+            if (VH_LOCK.compareAndSet(segment, 0L, index, current, current + 1)) {
+                return true;
+            }
+        }
     }
 
-    public boolean isWriteLocked(long index) {
-        return (int) VH_LOCK.get(segment, 0L, index) == 1;
+    void readUnlock(int index) {
+       // decrement reader counter
+       VH_LOCK.getAndAdd(segment, 0L, index, -1);
+    }
+
+    boolean tryWriteLock(long index) {
+        return (int) VH_LOCK.compareAndExchange(segment, 0L, index, 0, -1) == 0;
+    }
+
+    boolean isWriteLocked(long index) {
+        return (int) VH_LOCK.get(segment, 0L, index) == -1;
+    }
+
+    void backoff(int spins) {
+        if (spins < 64) {
+            Thread.onSpinWait();
+        } else {
+            LockSupport.parkNanos(1);
+        }
     }
 
     // ----- getter/setter -----
