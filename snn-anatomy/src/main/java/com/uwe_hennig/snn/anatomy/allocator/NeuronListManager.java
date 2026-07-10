1ec82f61d1b5f9088e3469953754204d66e784f4
UweHennig <uh@uwe-hennig.com>
2026-07-10 21:08:43 +0200

    adding NeuronListManager

---
+++ snn-anatomy/src/main/java/com/uwe_hennig/snn/anatomy/allocator/NeuronListManager.java
@@ -0,0 +1,61 @@
+/**
+ * @(#)NeuronListManager.java
+ * Copyright (c) 2026 Uwe Hennig
+ * All rights reserved.
+ */
+package com.uwe_hennig.snn.anatomy.allocator;
+
+import com.uwe_hennig.snn.anatomy.core.MultiList;
+
+/**
+ * NeuronListManager
+ *
+ * @author Uwe Hennig
+ */
+public class NeuronListManager {
+    private static NeuronListManager INSTANCE;
+    private MultiList                multiList;
+
+    private NeuronListManager(long maxBlocks, int minDataCapacityBytes) {
+        multiList = new MultiList(maxBlocks, minDataCapacityBytes);
+    }
+
+    public NeuronListManager init(long maxBlocks, int minDataCapacityBytes) {
+        if (INSTANCE == null) {
+            synchronized (AxonSynapseModelManager.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new NeuronListManager(maxBlocks, minDataCapacityBytes);
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    public static NeuronListManager instance() {
+        return INSTANCE;
+    }
+
+    public int nextId() {
+        return (int) multiList.allocate();
+    }
+
+    public MultiList getModel() {
+        return multiList;
+    }
+
+    public static void close() {
+        if (INSTANCE != null) {
+            INSTANCE.multiList.close();
+            INSTANCE.multiList = null;
+            INSTANCE = null;
+        }
+    }
+
+    public void save(String folder) {
+        /* TODO: Save model */
+    }
+
+    public void load(String folder) {
+        /* TODO Load model */
+    }
+}
